89d07a82ea6f9eab0819e7b95c7533a71e09d7fb
lapis <ceylinkarabulut08@gmail.com>
2026-05-10 21:08:06 +0300

    fixing the skeleton

--- src/main/java/com/objectville/cell/AbstractCell.java
+++ src/main/java/com/objectville/cell/AbstractCell.java
@@ -1,4 +1,21 @@
 package com.objectville.cell;
 
-public class AbstractCell {
+import com.objectville.grid.Position;
+
+public abstract class AbstractCell {
+
+    private final Position position;
+    private final CellType type;
+
+    protected AbstractCell(Position position, CellType type) {
+        this.position = position;
+        this.type = type;
+    }
+
+    public Position getPosition() { return position; }
+    public CellType getType() { return type; }
+
+    public abstract boolean isConnectable();
+    public abstract void resetTick();
+    public abstract String getLabel();
 }
