cf0fe8cb1dea52fdc2a58ff1ae61ec1cd3d57b87
lapis <ceylinkarabulut08@gmail.com>
2026-06-05 18:35:22 +0300

    JAR? Fixed?

--- src/main/java/com/objectville/Main.java
+++ src/main/java/com/objectville/Main.java
@@ -5,7 +5,7 @@ import com.objectville.simulation.SimulationEngine;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = args.length > 0 ? args[0] : "C:\\Users\\idilk\\IdeaProjects\\Objectville-II-3\\map00.txt";
+        String filePath = args.length > 0 ? args[0] : "map00.txt";
         int ticks = args.length > 1 ? Integer.parseInt(args[1]) : 12; //To handle at least 10 operations ("DON'T CHANGE THE  AND THE ARGUMENTS PART(IT'S HOW IT SHOULD BE!!)"
 
         Grid grid = Grid.loadFromFile(filePath);
