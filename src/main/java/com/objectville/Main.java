4f66fe2306d834f9b8d99abf4f2c329cea6bc519
idilkizilcelik <mariaidilka13@icloud.com>
2026-06-05 16:28:57 +0300

    Optimized the jar to at least 10 ticks.

--- src/main/java/com/objectville/Main.java
+++ src/main/java/com/objectville/Main.java
@@ -5,8 +5,10 @@ import com.objectville.simulation.SimulationEngine;
 
 public class Main {
     public static void main(String[] args) {
-        Grid grid = Grid.loadFromFile(args[0]);
-        int ticks = Integer.parseInt(args[1]);
+        String filePath = args.length > 0 ? args[0] : "C:\\Users\\idilk\\IdeaProjects\\Objectville-II-3\\map00.txt";
+        int ticks = args.length > 1 ? Integer.parseInt(args[1]) : 12; //To handle at least 10 operations ("DON'T CHANGE THE  AND THE ARGUMENTS PART(IT'S HOW IT SHOULD BE!!)"
+
+        Grid grid = Grid.loadFromFile(filePath);
         SimulationEngine engine = new SimulationEngine(grid, ticks);
         engine.run();
     }
