fa227d52a13e416dba2ee8c86ddbe936033023fb
lapis <ceylinkarabulut08@gmail.com>
2026-06-05 21:19:11 +0300

    optimized output.

--- src/main/java/com/objectville/simulation/TickManager.java
+++ src/main/java/com/objectville/simulation/TickManager.java
@@ -40,20 +40,20 @@ public class TickManager {
             int prev = h.getPreviousLevel();
             h.computeOutput();
 
-            System.out.println("House at: (" + h.getPosition().getRow() + "," + h.getPosition().getCol() + ")generated " + h.getOutput() + " population ");
+            System.out.println("House at (" + h.getPosition().getRow() + "," + h.getPosition().getCol() + ")generated " + h.getOutput() + " population ");
             printLevelChange("House:", h.getPosition(), prev, h.getLevel());
         }
         for (Industrial i : grid.getIndustrialZones()) {
             int prev = i.getPreviousLevel();
             i.computeOutput();
-            System.out.println("Industrial at :(" + i.getPosition().getRow() + "," + i.getPosition().getCol() + ") generated  " + i.getOutput() + "  goods");
+            System.out.println("Industrial at (" + i.getPosition().getRow() + "," + i.getPosition().getCol() + ") generated  " + i.getOutput() + "  goods");
             printLevelChange("Industrial: ", i.getPosition(), prev, i.getLevel());
         }
         for (Commercial c : grid.getCommercialZones()) {
             int prev = c.getPreviousLevel();
             c.computeOutput();
-            System.out.println("Commercial at:   (" + c.getPosition().getRow() + "," + c.getPosition().getCol() + ") generated  " + c.getOutput() + " lifestyle ");
-            printLevelChange("Commercial : ", c.getPosition(), prev, c.getLevel());
+            System.out.println("Commercial at   (" + c.getPosition().getRow() + "," + c.getPosition().getCol() + ") generated  " + c.getOutput() + " lifestyle ");
+            printLevelChange("Commercial", c.getPosition(), prev, c.getLevel());
         }
     }
 
@@ -65,7 +65,7 @@ public class TickManager {
 
     private static void printLevelChange(String name, com.objectville.grid.Position pos, int prev, int curr) {
         if (curr > prev) {
-            System.out.println(name + " at (" + pos.getRow() + "," + pos.getCol() + ") levels up from: " + prev + " to:" + curr);
+            System.out.println(name + " at (" + pos.getRow() + "," + pos.getCol() + ") levels up from " + prev + " to " + curr);
         } else if (curr < prev) {
             System.out.println(name + " at (" + pos.getRow() + "," + pos.getCol() + ") levels down from " + prev + " to " + curr);
         }
